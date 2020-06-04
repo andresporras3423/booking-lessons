fe620b5e32c2252691676e75d5cda92f6e08c609
andresporras3423 <andresporras3423@gmail.com>
2020-06-04 13:35:38 -0500

    add custom validation for requiring that subject of the lesson has to be a tutor's subject

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,13 +4,18 @@ class Lesson < ApplicationRecord
     belongs_to :user
     belongs_to :tutor, class_name: 'User', foreign_key: 'tutor_id'
     belongs_to :subject
-    validate :student_tutor_different_users
+    validate :student_tutor_different_users, :is_tutor_subject
     
     private
     def student_tutor_different_users
-        p "SUCH A WONDERFUL WORLD"
         if user_id==tutor_id
             errors.add(:base, message: "student and tutor cannot be the same person")
+          end
+    end
+
+    def is_tutor_subject
+        if User.all.find(tutor_id).userSubjects.none?{|us| us.subject_id==subject_id}
+            errors.add(:base, message: "subject must be a tutor subject")
           end
     end
 end
