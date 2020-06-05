93a51a1baf8d6844f5072690da43a30d04216f69
andresporras3423 <andresporras3423@gmail.com>
2020-06-05 17:47:38 -0500

    update http type for delete session

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post  '/sessions/create',  to: 'sessions#create'
-  get  '/sessions/delete',  to: 'sessions#destroy'
+  delete  '/sessions/delete',  to: 'sessions#destroy'
 
   post  '/users/create',  to: 'users#create'
   post  '/users/update_subjects',  to: 'users#update_subjects'
