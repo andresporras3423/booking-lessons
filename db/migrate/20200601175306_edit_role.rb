9cdcaee95699d692d0fbb55c1b68e6f5a1c34163
andresporras3423 <andresporras3423@gmail.com>
2020-06-09 20:37:18 -0500

    add documentations for all the actions of the controllers

--- db/migrate/20200601175306_edit_role.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class EditRole < ActiveRecord::Migration[6.0]
-  def change
-    add_column :roles, :description, :string
-  end
-end
--- test/models/user_subject_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class UserSubjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
